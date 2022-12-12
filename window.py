ead258514c5f9a1fef20bb8fd8dfd121ab3564aa
Ihor Mishchenya <Mishchenya.igor@gmail.com>
2022-12-12 06:23:49 +0200

    На этот раз точно заебись

--- window.py
+++ window.py
@@ -5,9 +5,9 @@ temp=0
 after_id=''
 def tick():
     global temp,after_id
-    after_id=root.after(1000,tick)
+    after_id = root.after(1000,tick)
     f_temp = datetime.fromtimestamp(temp).strftime("%M:%S")
-    timer.configure(text=(str(f_temp)))
+    timer.configure(text = (str(f_temp)))
     temp+=1
 def start_timer():
     tick()
@@ -20,49 +20,62 @@ def stop_timer():
 root = Tk()
 root.title('SpamBot')
 root.geometry('1264x720')
-root.resizable(width=False,height=False)
-root.image = PhotoImage(file=("pictures/backgraund.png"))
-bg= Label(root, image=root.image)
-bg.grid(row=0, column=0)
+root.resizable(width = False, height = False)
+root.image = PhotoImage(file = ("pictures/backgraund.png"))
+bg= Label(root, image = root.image)
+bg.grid( row = 0, column = 0)
 
 
 # <----------Top Name-------------->
 t1 = Label(root, text='Enter yout text here:', fg='white', background='#2c2c2c')
-t1.config(font=('Comic Sans MS',20))
-t1.place(x=365,y=20)
+t1.config(font = ('Comic Sans MS', 20))
+t1.place(x = 365, y = 20)
 
 
 # <----------Timer-------------->
-timer = Label(root,text="00:00",fg="white",background='#2c2c2c')
-timer.config(font=('Verdana',30))
-timer.place(x=450,y=600)
+timer = Label(root,text = "00:00", fg = "white", background = '#2c2c2c')
+timer.config(font = ('Verdana',30))
+timer.place(x = 450, y = 600)
 
 
 
 # <----------Text edit-------------->
 edit = Text(root)
-edit.config(font=('Comic Sans MS',15),fg='white',background='#646363')
+edit.config(font = ('Comic Sans MS', 15),fg = 'white', background = '#646363')
 edit['border'] = 0
-edit.place(x=265,y=75, width=500,height=400)
+edit.place(x = 265,y = 75, width = 500, height = 400)
 
 
 # <----------Button Start-------------->
-button_start_images = PhotoImage(file=("pictures/button_start.png"))
-button_start = Button(root, image=button_start_images)
-button_start.config(background="#2c2c2c")
+button_start_images = PhotoImage(file = ("pictures/button_start.png"))
+button_start = Button(root, image = button_start_images)
+button_start.config(background = "#2c2c2c")
 button_start["border"] = 0
-button_start.place(x=314,y=500,width=100,height=60)
+button_start.place(x = 314, y = 500, width = 100, height = 60)
 
 
 # <----------Button Stop-------------->
-button_stop_images = PhotoImage(file=("pictures/button_stop.png"))
-button_stop = Button(root,image=button_stop_images, command=start_timer)
-button_stop.config(background="#2c2c2c")
+button_stop_images = PhotoImage(file = ("pictures/button_stop.png"))
+button_stop = Button(root,image = button_stop_images, command = start_timer)
+button_stop.config(background = "#2c2c2c")
 button_stop["border"] = 0
-button_stop.place(x=614,y=500,width=100,height=60)
+button_stop.place(x = 614, y = 500, width = 100, height = 60)
 
 
+# <----------View anckets-------------->
+quantity_anket=3
+count=0
+coordinate_y=6
+if quantity_anket > 0:
+    for i in range(quantity_anket):
+        view_ankets = Frame(root, width = 220, height = 60, highlightbackground = "#6ec261", highlightthickness = 3,
+                             background = '#646363')
+        view_ankets.place(x = 7, y = coordinate_y)
+        coordinate_y += 70
 
+# <----------View balance-------------->
 
-
+view_balance = Label(root, text = "Balance:")
+view_balance.config(font = ('Comic Sans MS', 15),fg  = "#6ec261", background  = '#2c2c2c')
+view_balance.place(x = 15,y = 680)
 
